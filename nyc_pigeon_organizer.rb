41b478260e068637838de4e77f0dedc2574fe719
Ricardo Persaud <ricardo39985@gmail.com>
2019-11-23 20:28:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,11 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  result = {}
+  for i in data[:gender][:male]
+    result[i]={}
+  end
+  for i in data[:gender][:female]
+    result[i]={}
+  end
+  pp result
 end
